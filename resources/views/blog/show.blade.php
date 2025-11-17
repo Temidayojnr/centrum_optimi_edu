61bd7d339afb260686e53685672d0966450765d0
temidayojnr <temidayojunior@gmail.com>
2025-11-17 10:00:22 +0100

    Client fixes

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -31,13 +31,6 @@
                     {{ $post->author->name }}
                 </span>
                 @endif
-                <span class="flex items-center">
-                    <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                    </svg>
-                    {{ $post->views ?? 0 }} views
-                </span>
             </div>
             <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-white mb-6 leading-tight">
                 {{ $post->title }}
